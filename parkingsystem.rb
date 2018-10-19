1967a26c699a107cf3213741c59aa3d5ba0d972c
kevinreyyonathan <kevinryonathan@gmail.com>
2018-10-20 04:29:39 +1100

    parkingsystem class updated

--- parkingsystem.rb
+++ parkingsystem.rb
@@ -54,3 +54,48 @@ class ParkingSystem
 	end
 
 	
+	def slot_no_by_color(color)
+		result = parking_lot.get_slot_no_by_color(color)
+		size = result.size
+		compact_to_string(size, result)
+	end
+
+
+	def slot_no_by_registration_no(reg_no)
+		slot_no = parking_lot.get_slot_no_by_registration_no(reg_no)
+		return 'Not found' unless slot_no
+		slot_no.to_s
+	end
+	
+	def print_table
+		parking_slots = parking_lot.slots
+		puts "Slot No. \t Registration No. \t Color"
+		parking_slots.each_with_index do |slot, idx|
+			next unless slot
+			output = (idx+1).to_s + "\t\t" + slot.reg_no + "\t\t" + slot.color
+			puts output
+		end
+	end
+
+	def park_on_slot(reg_no:, color:, slot_no:)
+		car = Car.new(reg_no: reg_no, color: color)
+		parking_lot.park(car: car, slot_no: slot_no)
+	end
+
+	
+	def park_check(reg_no:, color:, slot_no:)
+		if slot_no
+			park_on_slot(reg_no: reg_no, color: color, slot_no: slot_no)
+			print_result 'Allocated slot number: ' + (slot_no + 1).to_s
+		else
+			print_result 'Sorry, the parking lot is currently full'
+		end
+	end
+	
+	
+	def leave_process(num_in_str)
+		num_in_int = str_to_int(num_in_str)
+		leave_park_slot(num_in_int - 1)
+		print_result('Slot number ' + num_in_str + ' is free')
+	end
+	
