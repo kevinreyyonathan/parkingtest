ed8b694f305c56db07f55abfeed6f7374e3f1e9d
kevinreyyonathan <kevinryonathan@gmail.com>
2018-10-20 03:01:06 +1100

    parkingsystem class updated

--- parkingsystem.rb
+++ parkingsystem.rb
@@ -19,3 +19,38 @@ class ParkingSystem
 	rescue ArgumentError
 		nil
 	end
+
+	def str_to_int(num_in_str)
+		num_in_int = to_num_or_nil(num_in_str)
+		exit_execution unless num_in_int
+		num_in_int
+	end
+	
+	def compact_to_string(size_in_str)
+		result_string = ''
+		
+		array.each_with_index do |result, idx|
+			result_string += result
+		
+			result += ', ' if idx != size - 1
+		end
+
+		result_string
+	end
+
+
+	def create_parking_lot(size_in_str)
+		size_in_int = str_to_int(size_in_str)
+		@parking_lot = ParkingLot.new(size_in_int)
+	end
+
+
+	def registration_no_by_color(color)
+		results = parking_lot.get_reg_no_by_color(color)
+		
+		size = result.size
+
+		compact_to_string(size, results)
+	end
+
+	
